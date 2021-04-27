7bc1418a77a29017c526cb78f6dc80d9a0df55fe
Zylanx <ZylanxDragon@gmail.com>
2021-04-28 08:15:38 +1000

    Add files via upload

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import RPi.GPIO as gpio
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.togglebutton import ToggleButton
+from kivy.uix.button import Button
 
 
 RED_LED_PIN = 29
@@ -10,6 +11,8 @@ ORANGE_LED_PIN = 31
 GREEN_LED_PIN = 33
 
 
+# We are using toggle buttons since they are a lot more appropriate for this.
+# Infact this is what they are made for.
 class LedWidget(ToggleButton):
     def __init__(self, led_pin, **kwargs):
         super().__init__(**kwargs)
@@ -25,19 +28,29 @@ class LedWidget(ToggleButton):
             gpio.output(self.led_pin, gpio.LOW)
 
 
+
 class LedScreen(GridLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
         self.cols = 3
 
-        red_led = LedWidget(RED_LED_PIN)
-        orange_led = LedWidget(ORANGE_LED_PIN)
-        green_led = LedWidget(GREEN_LED_PIN)
+        red_led = LedWidget(RED_LED_PIN, text="Red LED", group="LEDs")
+        orange_led = LedWidget(ORANGE_LED_PIN, text="Orange LED", group="LEDs")
+        green_led = LedWidget(GREEN_LED_PIN, text="Green LED", group="LEDs")
+
+        exit_button = Button(text="Exit")
+        exit_button.bind(on_release=self.exit_app)
 
         self.add_widget(red_led)
         self.add_widget(orange_led)
         self.add_widget(green_led)
+
+        self.add_widget(exit_button)
+
+
+    def exit_app(self):
+        App.get_running_app().stop()
 
 
 class LedApp(App):
@@ -49,3 +62,5 @@ if __name__ == '__main__':
     gpio.setmode(gpio.BOARD)
 
     LedApp().run()
+
+    gpio.cleanup()
